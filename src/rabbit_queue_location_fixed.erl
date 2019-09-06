b23a1281f3d85fbbdb154ff6e6a6f8da3481abcc
Grigory Starinkin <starinkin@gmail.com>
2019-09-06 16:21:15 -0500

    init commit

---
+++ src/rabbit_queue_location_fixed.erl
@@ -0,0 +1,26 @@
+-module(rabbit_queue_location_fixed).
+
+-behaviour(rabbit_queue_master_locator).
+
+-include("rabbit.hrl").
+
+-export([description/0, queue_master_location/1]).
+
+-rabbit_boot_step({?MODULE,
+                   [{description, "locate queue master with a configured node"},
+                    {mfa,         {rabbit_registry, register,
+                                   [queue_master_locator,
+                                    <<"fixed">>, ?MODULE]}},
+                    {requires,    rabbit_registry},
+                    {enables,     kernel_ready}]}).
+
+
+%%---------------------------------------------------------------------------
+%% Queue Master Location Callbacks
+%%---------------------------------------------------------------------------
+
+description() ->
+    [{description, <<"Place master queue on a configured node.">>}].
+
+queue_master_location(#amqqueue{}) ->
+    {ok, application:get_env(rabbit, master_node_location, node())}.
